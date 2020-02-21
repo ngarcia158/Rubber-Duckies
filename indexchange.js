92b20aa869505c05ad4516388adde17a6327ce45
Nicholas Garcia <ngarcia158@west-mec.org>
2020-02-21 13:46:04 -0700

    Added changing buttons

---
+++ indexchange.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+function run1(){
+    var color = "index.css";
+    document.getElementById("css").href = color;
+
+}
+
+function run2(){
+    var color = "index2.css";
+    document.getElementById("css").href = color;
+}
+
+function run3(){
+    var color = "index3.css";
+    document.getElementById("css").href = color;
+}
+
+function run4(){
+    var color = "index4.css";
+    document.getElementById("css").href = color;
+}
